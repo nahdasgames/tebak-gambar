866030e4495cca1fd9aed63ed229886a39ac60ad
ariaresa <ariaresa93@gmail.com>
2021-04-01 17:31:08 +0700

    tambah getHasil pada script.js

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-const pilihan = document.querySelector('.nama, .gambar');
+function getHasil(name, pict) {
+	if (name == 'nama' && pict == 'gambar') {alert('oke');} else {alert('maaf salah')};
+};
 
-pilihan.addEventListener('click', function(nama, gambar){
-		const getNama = nama.className;
-		const getGambar = gambar.className;
-		// console.log(nama);	
-		if(nama == 'nama' || gambar == 'gambar') {
-		alert('anda menang');	
-	}
-})
-
-const gambar = document.querySelector('.gambar');
-
-gambar.addEventListener('click', function(){
-		const getGambar = gambar.className;
-		console.log(getGambar);
-	})
-
-// function getHasil(nama, gambar) {
-// 	if(nama == 'nama' && gambar == 'gambar') {
-// 		alert('anda menang');
-// 	} 
+// function pilNama(e) {
+// 	document.querySelector('.nama');
+// 	const getNama = e.target.className;
+// 	// console.log(getNama);
+// 	// return getNama;
 // }
 
-// getHasil(getNama,getGambar);
+const pilihan = document.querySelectorAll('.pilihan');
+pilihan.forEach(function(nama) {
+	nama.addEventListener('click', function(){
+	// document.querySelector('.gambar');
+	const tes = document.querySelector('.nama');
+	const tos = document.querySelector('.gambar');
+	const getNama = tes.className;
+	const getGambar = tos.className;
+
+	getHasil(getNama, getGambar);
+	});	
+})
+
+
+
+
+// function pilGambar(e) {
+// 	document.querySelector('.gambar');
+// 	const getGambar = e.target.className;
+// 		// console.log(getGambar);
+// 		return getGambar;
+// }
+
+// const gambar = document.querySelector('.gambar');
+// 	gambar.addEventListener('click', pilGambar);
+
+
