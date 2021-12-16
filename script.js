f4d6bcf9621dde2f3c56744c414cb3b283772f2d
primagamangagel <ngagelprimagama@gmail.com>
2021-12-16 21:27:57 +0700

    ubah font dan icon navigasi

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ const soalHewan = document.querySelectorAll('.soal-hewan');
 const hwSatu = document.querySelector('#hw-satu');
 const hwDua = document.querySelector('#hw-dua');
 const hwTiga = document.querySelector('#hw-tiga');
+const hwEmpat = document.querySelector('#hw-empat');
+const soalBuah = document.querySelectorAll('.soal-buah');
+const buSatu = document.querySelector('#bu-satu');
+const buDua = document.querySelector('#bu-dua');
+const buTiga = document.querySelector('#bu-tiga');
+const buEmpat = document.querySelector('#bu-empat');
 
 
 btnHewan.addEventListener('click', function () {
 	menu.setAttribute("hidden", true);
-
 	hwSatu.removeAttribute("hidden");
 })
 
 btnBuah.addEventListener('click', function () {
-	btnHewan.setAttribute("hidden", true);
-	btnBuah.setAttribute("hidden", true);
+	menu.setAttribute("hidden", true);
+	buSatu.removeAttribute("hidden");
 })
 
 function getHasil(e) {
@@ -43,36 +48,71 @@ btnKeluar.forEach(function (keluar) {
 		for (let hewan of soalHewan) {
 			hewan.setAttribute("hidden", true);
 		}
+
+		for (let buah of soalBuah) {
+			buah.setAttribute("hidden", true);
+		}
 	})
 })
+
+function nextPrev(hilang, muncul) {
+	hilang.setAttribute("hidden", true);
+	muncul.removeAttribute("hidden");
+}
 
 btnNext.forEach(function (next) {
 	next.addEventListener('click', function () {
 		if (next.value === "next1") {
-			hwSatu.setAttribute("hidden", true);
-			hwDua.removeAttribute("hidden");
+			nextPrev(hwSatu, hwDua);
 		} else if (next.value === "next2") {
-			hwDua.setAttribute("hidden", true);
-			hwTiga.removeAttribute("hidden");
+			nextPrev(hwDua, hwTiga);
 		} else if (next.value === "next3") {
-			hwTiga.setAttribute("hidden", true);
-			hwSatu.removeAttribute("hidden");
+			nextPrev(hwTiga, hwEmpat);
+		} else if (next.value === "next4") {
+			nextPrev(hwEmpat, hwSatu);
 		}
 	})
-
 })
 
 btnPrev.forEach(function (prev) {
 	prev.addEventListener('click', function () {
 		if (prev.value === "prev1") {
-			hwSatu.setAttribute("hidden", true);
-			hwTiga.removeAttribute("hidden");
+			nextPrev(hwSatu, hwEmpat);
 		} else if (prev.value === "prev2") {
-			hwDua.setAttribute("hidden", true);
-			hwSatu.removeAttribute("hidden");
+			nextPrev(hwDua, hwSatu);
 		} else if (prev.value === "prev3") {
-			hwTiga.setAttribute("hidden", true);
-			hwDua.removeAttribute("hidden");
+			nextPrev(hwTiga, hwDua);
+		} else if (prev.value === "prev4") {
+			nextPrev(hwEmpat, hwTiga);
+		}
+	})
+
+})
+
+btnNext.forEach(function (next) {
+	next.addEventListener('click', function () {
+		if (next.value === "next5") {
+			nextPrev(buSatu, buDua);
+		} else if (next.value === "next6") {
+			nextPrev(buDua, buTiga);
+		} else if (next.value === "next7") {
+			nextPrev(buTiga, buEmpat);
+		} else if (next.value === "next8") {
+			nextPrev(buEmpat, buSatu);
+		}
+	})
+})
+
+btnPrev.forEach(function (prev) {
+	prev.addEventListener('click', function () {
+		if (prev.value === "prev5") {
+			nextPrev(buSatu, buEmpat);
+		} else if (prev.value === "prev6") {
+			nextPrev(buDua, buSatu);
+		} else if (prev.value === "prev7") {
+			nextPrev(buTiga, buDua);
+		} else if (prev.value === "prev8") {
+			nextPrev(buEmpat, buTiga);
 		}
 	})
 
